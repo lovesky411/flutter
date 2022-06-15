5ba23137996048ebc4042f9fe26ea89cce0942a3
nomad411 <nomad411@naver.com>
2022-06-15 22:36:28 +0900

    Update home_screen.dart
    오늘도 출근 - 구글지도 사용, 앱권한 관리 추가

--- check/lib/screen/home_screen.dart
+++ check/lib/screen/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,12 +10,93 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  static final LatLng companyLatLng = LatLng(
+      37.5233273,
+      126.921252
+  );
+
+  static final CameraPosition initialPosition = CameraPosition(
+    target: companyLatLng,
+    zoom: 15
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Screen'),
+      appBar: renderAppBar(),
+      body: Column(
+        children: [
+          _CustomGoogleMap(
+              initialPosition: initialPosition
+          ),
+          _ChoolCheckButton(),
+        ],
+      )
+    );
+  }
+
+  Future<String> checkPermission() async {
+    final isLocationEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if(!isLocationEnabled){
+      return '위치 서비스를 활성화 해주세요.';
+    }
+
+    LocationPermission checkedPermission = await Geolocator.checkPermission();
+
+    if(checkedPermission == LocationPermission.denied){
+      checkedPermission == await Geolocator.requestPermission();
+
+      if(checkedPermission == LocationPermission.denied){
+        return '위치 권한을 허가해주세요.';
+      }
+    }
+
+    if(checkedPermission == LocationPermission.deniedForever){
+      return '앱의 위치 권한을 세팅에서 허가해주세요.';
+    }
+
+    return '위치권한을 허가해주세요.';
+  }
+
+  AppBar renderAppBar(){
+    return AppBar(
+      title: Text(
+        '오늘도 출근',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
+        ),
       ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
+
+class _CustomGoogleMap extends StatelessWidget {
+  final CameraPosition initialPosition;
+  const _CustomGoogleMap({required this.initialPosition, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Expanded(
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: initialPosition,
+
+      ),
+    );
+  }
+}
+
+class _ChoolCheckButton extends StatelessWidget {
+  const _ChoolCheckButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Expanded(
+      child: Text('출근'),
     );
   }
 }
